c35a7ee6546b54795683d7dde8e10fd0f669028b
Miguel Angel Ramos Castaño <miguel.ramos.mt@correo.usa.edu.co>
2021-12-01 16:24:45 -0500

    Reto 2 Fronted 80%

--- js/funciones.js
+++ js/funciones.js
@@ -20,7 +20,7 @@ $("#login").click(function(){
 
 function validarUsuario(response) {
     if(response.id != null){
-        location.href = "bienvenida.html";
+        location.href = "usuarios.html";
         alert("¡Bienvenido! "+ response.name);
 
         var bienvenida = document.getElementById("#bienvenido");
@@ -28,7 +28,7 @@ function validarUsuario(response) {
         texto.append("Hola");
         bienvenida.append(texto);
     }else{
-        alert("Usuario no registrado");
+        alert("Usuario no registrado :/");
     }
 }
 
@@ -39,31 +39,41 @@ $("#guardar").click(function(){
     }else{
         if($("#contrasenaRegistro").val() == $("#contrasenaRegistro2").val()){
             let datos = {
+                id: $('#id').val(),
+                identification: $('#id').val(),
+                name: $("#usuarioRegistro").val(),
+                address: "",
+                cellPhone: "",
                 email: $("#emailRegistro").val(),
                 password: $("#contrasenaRegistro").val(),
-                name: $("#usuarioRegistro").val()
+                zone: "",
+                type: ""
             }
+            datos=JSON.stringify(datos);
             $.ajax({
                 url:"http://localhost:8080/api/user/new",
                 method:"POST",
-                dataType:"json",
-                data:JSON.stringify(datos),
+                dataType:"JSON",
+                data:datos,
                 contentType:"application/json",
-                Headers:{
-                    "Content-Type":"application/json"
+                success:function(response){
+                    console.log(response);
                 },
                 statusCode: {
                     201: function(response){
                         console.log(response);
-                        alert("Registrado Correctamente :O ");
+                        alert("Registrado Correctamente");
                     }
                 }
             });
         }else{
             alert("Las contraseñas no coinciden :c");
+            $("#contrasenaRegistro").val("");
+            $("#contrasenaRegistro2").val("");
         }
     }
 });
+
 
 function validarEmail(){
     let datos = {
@@ -78,3 +88,40 @@ function validarEmail(){
         }
     });
 }
+
+
+$("#guardarInventario").click(function(){
+    if($.trim($("#brandRegistro").val()) == "" || $.trim($("#procesorRegistro").val()) == "" || $.trim($("#osRegistro").val()) == "" || $.trim($("#descriptionRegistro").val()) == "" || $.trim($("#memoryRegistro").val()) == "" || $.trim($("#hardDriveRegistro").val()) == "" || $.trim($("#availabilityRegistro").val()) == "" || $.trim($("#priceRegistro").val()) == "" || $.trim($("#quantityRegistro").val()) == "" || $.trim($("#photographyRegistro").val()) == ""){
+        alert("Por favor ingrese todos los campos");
+    }else{
+            let datos = {
+                id: $("#idInventario").val(),
+                brand: $("#brandRegistro").val(),
+                procesor: $("#procesorRegistro").val(),
+                os: $("#osRegistro").val(),
+                description: $("#descriptionRegistro").val(),
+                memory: $("#memoryRegistro").val(),
+                hardDrive: $("#hardDriveRegistro").val(),
+                availability: $("#availabilityRegistro").val(),
+                price: $("#priceRegistro").val(),
+                quantity: $("#quantityRegistro").val(),
+                photography: $("#photographyRegistro").val()
+            }
+            $.ajax({
+                url:"http://localhost:8080/api/clone/new",
+                method:"POST",
+                dataType:"JSON",
+                data:JSON.stringify(datos),
+                contentType:"application/json",
+                Headers:{
+                    "Content-Type":"application/json"
+                },
+                statusCode: {
+                    201: function(response){
+                        console.log(response);
+                        alert("Registrado Correctamente");
+                    }
+                }
+            });
+    }
+});
